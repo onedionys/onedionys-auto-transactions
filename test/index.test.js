0f3fe752e253499197f6a71b5259c0674ad80e02
onedionys <onedionys@gmail.com>
2025-04-11 10:37:29 +0700

    test: add initial test suite for core functions using chai and ethereum-waffle

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+import { use } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { solidity } from 'ethereum-waffle';
+
+use(solidity);
+use(chaiAsPromised);
+
+describe('Testing Functions', function () {
+    it('Should normal', async function () {});
+});
